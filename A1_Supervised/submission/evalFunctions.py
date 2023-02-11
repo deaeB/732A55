2c8723827fe1527f2ec23d4b8cdeaca528c46ffe
NI D.W <deaeb@hotmail.com>
2023-02-11 16:36:27 +0100

    lab 1 submission

---
+++ A1_Supervised/submission/evalFunctions.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+def calcAccuracy(LPred, LTrue):
+    """Calculates prediction accuracy from data labels.
+
+    Args:
+        LPred (array): Predicted data labels.
+        LTrue (array): Ground truth data labels.
+
+    Retruns:
+        acc (float): Prediction accuracy.
+    """
+
+    # --------------------------------------------
+    # === Your code here =========================
+    # --------------------------------------------
+    acc = sum(LPred == LTrue) / len(LTrue)
+    # ============================================
+    return acc
+
+
+def calcConfusionMatrix(LPred, LTrue):
+    """Calculates a confusion matrix from data labels.
+
+    Args:
+        LPred (array): Predicted data labels.
+        LTrue (array): Ground truth data labels.
+
+    Returns:
+        cM (array): Confusion matrix, with predicted labels in the rows
+            and actual labels in the columns.
+    """
+
+    # --------------------------------------------
+    # === Your code here =========================
+    # --------------------------------------------
+    classes = np.unique(LTrue)
+    # array 1*d of d classes
+    nClasses = classes.shape[0]
+    # how much classes we have
+
+    cM  = np.zeros((nClasses,nClasses),dtype=int)
+    np.add.at(cM, (LPred, LTrue), 1)
+    # ============================================
+
+    return cM
+
+
+def calcAccuracyCM(cM):
+    """Calculates prediction accuracy from a confusion matrix.
+
+    Args:
+        cM (array): Confusion matrix, with predicted labels in the rows
+            and actual labels in the columns.
+
+    Returns:
+        acc (float): Prediction accuracy.
+    """
+
+    # --------------------------------------------
+    # === Your code here =========================
+    # --------------------------------------------
+    acc = np.trace(cM) / np.sum(cM)
+    # ============================================
+    
+    return acc
